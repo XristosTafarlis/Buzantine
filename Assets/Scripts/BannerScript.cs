d306c5d6241716879a86b3111272559a1a9b58b3
Χρήστος Ταφαρλής <xristos.p.tafarlis@gmail.com>
2022-07-25 21:09:40 +0300

    Added battle of Dara

--- Assets/Scripts/BannerScript.cs
+++ Assets/Scripts/BannerScript.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 public class BannerScript : MonoBehaviour{
 	bool flag = false;
 	void Update(){
-		
-		Debug.Log(transform.rotation.x);
 		if(transform.rotation.x < 0.05 && flag == false){
 			transform.Rotate (5f * Time.deltaTime, 5f * Time.deltaTime, 5f * Time.deltaTime);
 		}else{
