59bbb38daecd03b8f5374757d4c8c833696f9f56
Χρήστος Ταφαρλής <xristos.p.tafarlis@gmail.com>
2022-10-30 08:53:26 +0200

    Expanded firther the pause menu

--- Assets/Scripts/MainGamePauseMenu.cs
+++ Assets/Scripts/MainGamePauseMenu.cs
@@ -1,16 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainGamePauseMenu : MonoBehaviour{
 	[SerializeField] GameObject mainText;
 	[SerializeField] GameObject pauseText;
 	[SerializeField] GameObject player;
-	bool isPaused;
 	
+	[SerializeField] Text [] locationText;
+	bool isPaused;
+
 	void Update(){
 		PauseUnpase();
+		if(MainMapCanvasScript.hasWonSpaniae == true){
+			locationText[0].color = Color.green;
+		}
+		if(MainMapCanvasScript.hasWonItaliaAnnonaria == true){
+			locationText[1].color = Color.green;
+		}
+		if(MainMapCanvasScript.hasWonItaliaSuburbicaria == true){
+			locationText[2].color = Color.green;
+		}
+		if(MainMapCanvasScript.hasWonIllyricum == true){
+			locationText[3].color = Color.green;
+		}
+		if(MainMapCanvasScript.hasWonDacia == true){
+			locationText[4].color = Color.green;
+		}
+		if(MainMapCanvasScript.hasWonMacedonia == true){
+			locationText[5].color = Color.green;
+		}
+		if(MainMapCanvasScript.hasWonQuaesturaExercitus == true){
+			locationText[6].color = Color.green;
+		}
+		if(MainMapCanvasScript.hasWonThracia == true){
+			locationText[7].color = Color.green;
+		}
+		if(MainMapCanvasScript.hasWonPontica == true){
+			locationText[8].color = Color.green;
+		}
+		if(MainMapCanvasScript.hasWonAsiana == true){
+			locationText[9].color = Color.green;
+		}
+		if(MainMapCanvasScript.hasWonOriens == true){
+			locationText[10].color = Color.green;
+		}
+		if(MainMapCanvasScript.hasWonAegyptus == true){
+			locationText[11].color = Color.green;
+		}
+		if(MainMapCanvasScript.hasWonAfrica == true){
+			locationText[12].color = Color.green;
+		}
+		if(MainMapCanvasScript.hasWonSassanids == true){
+			locationText[13].color = Color.yellow;
+		}
+		if(MainMapCanvasScript.hasWonOstrogoths == true){
+			locationText[14].color = Color.yellow;
+		}
 	}
 	
 	void PauseUnpase(){
